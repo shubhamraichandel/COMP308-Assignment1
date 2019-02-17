c36b70759156dc8ba096387348b671116111e90d
Shubham Raichandel <shubhamraichandel@yahoo.com>
2019-02-17 00:08:08 -0500

    first commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,15 @@
+/* Custom JavaScript */
+// IIFE
+
+(function(){
+
+    function Start() {
+
+        console.log(`%c App Started...`, "font-size: 18px; color: green; font-weight:bold");
+    }
+
+
+
+    window.addEventListener("load",Start);
+
+})();
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
